823545964981f92f0389b4e683fb02301a084a2b
Tyler Distad <tyler.distad@gmail.com>
2017-10-30 11:02:07 -0400

    Force my custom aliases over oh-my-zsh

--- etc/zsh/init.zsh
+++ etc/zsh/init.zsh
@@ -1,4 +1,6 @@
 source ${DOTFILES}/etc/zsh/oh-my.zsh
+# re-apply my custom aliases
+source ${DOTFILES}/etc/sh/init.sh
 source ${DOTFILES}/etc/zsh/vars.zsh
 source ${DOTFILES}/etc/zsh/config.zsh
 source ${DOTFILES}/etc/zsh/iterm2.zsh
