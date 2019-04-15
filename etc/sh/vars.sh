b8ea21b33baa6e9c57e3c7dc50140db3a42ca2c2
Tyler Distad <distyler@amazon.com>
2019-04-15 18:31:38 +0000

    Inject either Linuxbrew/Homebrew into path but not both

--- etc/sh/vars.sh
+++ etc/sh/vars.sh
@@ -1,15 +1,25 @@
+# determine which OS we're running on
+case "$(uname -s)" in
+    Linux*)   DOTFILES_OS=Linux;;
+    Darwin*)  DOTFILES_OS=Mac;;
+esac
+
 # Global shell config variables
 export EDITOR=vim
 
 # Inject Homebrew into the PATH
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:${PATH}
-export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
+if [ "${DOTFILES_OS}" == Mac ]; then
+  export PATH=/usr/local/opt/coreutils/libexec/gnubin:${PATH}
+  export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}
+fi
 
 # Inject Linuxbrew into the PATH
-export PATH=/home/linuxbrew/.linuxbrew/bin:~/.linuxbrew/bin:${PATH}
-export PATH=/home/linuxbrew/.linuxbrew/sbin:~/.linuxbrew/sbin:${PATH}
-export MANPATH=/home/linuxbrew/.linuxbrew/share/man:~/.linuxbrew/share/man:${MANPATH}
-export INFOPATH=/home/linuxbrew/.linuxbrew/share/info:~/.linuxbrew/share/info:${INFOPATH}
+if [ "${DOTFILES_OS}" == Linux ]; then
+  export PATH=/home/linuxbrew/.linuxbrew/bin:~/.linuxbrew/bin:${PATH}
+  export PATH=/home/linuxbrew/.linuxbrew/sbin:~/.linuxbrew/sbin:${PATH}
+  export MANPATH=/home/linuxbrew/.linuxbrew/share/man:~/.linuxbrew/share/man:${MANPATH}
+  export INFOPATH=/home/linuxbrew/.linuxbrew/share/info:~/.linuxbrew/share/info:${INFOPATH}
+fi
 
 # History management
 export HISTFILE=~/.tmp/.histfile
