fc529c4987ef71183dd3f2e631deadb6b62fc4cd
Tyler Distad <distyler@amazon.com>
2019-10-04 13:51:30 +0000

    Add zsh-autosuggestions.zsh config

---
+++ etc/zsh/oh-my-zsh-custom/zsh-autosuggestions.zsh
@@ -0,0 +1,7 @@
+# https://github.com/zsh-users/zsh-autosuggestions#configuration
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=11"
+ZSH_AUTOSUGGEST_USE_ASYNC=true
+ZSH_AUTOSUGGEST_MANUAL_REBIND=true
+
+_zsh_autosuggest_bind_widgets
