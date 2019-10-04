e595df027b96acc9b4e4df3a582f699bdba4ce13
Tyler Distad <distyler@amazon.com>
2019-10-04 13:48:37 +0000

    Add nvm

--- etc/sh/config.sh
+++ etc/sh/config.sh
@@ -6,3 +6,8 @@ fi
 # prevent Ctrl-Q from mapping to `stty start`
 # allows <C-Q> to map to <C-V> in Vim
 stty start undef
+
+# enable nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
